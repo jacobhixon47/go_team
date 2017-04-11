0ad1bc952d11f24a93250ff2e0a9f933fa23c93b
Jacob Hixon <jacobhixon47@gmail.com>
2017-04-10 21:01:48 -0700

    layout homepage and sign up form setup

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,21 +1,37 @@
 $(document).on("turbolinks:load", function() {
-  $("#get-started-button-home").click(function() {
+  $("#learn-more-button-home").click(function() {
+    $('#signup-form').show();
+    $('#home-slideshow').show();
     $('html, body').animate({
         scrollTop: $("#home-slideshow").offset().top
     }, 1000);
   });
+  $("#get-started-button-home").click(function() {
+    $('#home-slideshow').show();
+    $('#signup-form').show();
+    $('html, body').animate({
+        scrollTop: $("#signup-form").offset().top
+    }, 1000);
+  });
+  $(".back-to-top").click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+  });
+
 
   var nav = $('.my-nav');
 	$(window).scroll(function() {
 		var scroll = $(window).scrollTop();
-
 		if (scroll >= 300 && !nav.hasClass('nav-light')) {
 			nav.slideUp(1, function() {
         nav.removeClass('nav-pop').addClass('nav-light').fadeIn(500);
+        $('.back-to-top').fadeIn(500);
       });
 		} else if (scroll <= 100 && !nav.hasClass('nav-pop')) {
       nav.slideUp(1, function() {
         nav.removeClass('nav-light').addClass('nav-pop').fadeIn(500);
+        $('.back-to-top').fadeOut(500);
       });
 		}
   });
